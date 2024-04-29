f8f38ea76381f30ca9ed5301ffa470a67828eab8
Corey Yates <cqyates@gmail.com>
2024-04-28 20:56:13 -0400

    created typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,22 @@ const typeDefs = `
   type User {
     _id: ID!
     username: String!
-    email: String
+    email: String!
+    admin: Boolean
+    techStack: [Tech]
+    projects: [Project]
+  }
+  type Project {
+    _id: ID!
+    title: String!
+    description: String!
+    githubLink: String!
+    deployedLink: String
+    imageName: String
+    tech: [Tech]
+  }
+  type Tech {
+    name: String!
   }
 
   type Auth {
@@ -12,12 +27,12 @@ const typeDefs = `
 
   type Query {
     me:User
-    users:[User]
+    tech: [Tech]
+    projects: [Project]
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
