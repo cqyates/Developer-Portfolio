13962bbf8b39c6906dd0454db43b2bac405f180f
Corey Yates <cqyates@gmail.com>
2024-04-29 12:46:20 -0400

    added QUERY USER to front end home page - tested

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,4 +1,15 @@
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
 const HomePage = () => {
+  //for signed in user to create edit and admin privileges
+
+  // const { loading, data } = useQuery(QUERY_ME);
+  // const userData = data?.me || {};
+
+  // for visitors to my website to see my work
+  const {loading, data} = useQuery(QUERY_USER)
+  const userData = data?.user || {}
+  console.log(userData)
   return (
     <div>
       <h1>Home Page Placeholder</h1>
