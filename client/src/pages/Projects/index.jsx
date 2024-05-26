cf563ae81a40f4c2fc8396efa438d78e948102f0
Corey Yates <cqyates@gmail.com>
2024-05-26 16:45:20 -0400

    added react-carousel

--- client/src/pages/Projects/index.jsx
+++ client/src/pages/Projects/index.jsx
@@ -1,16 +1,16 @@
-import {useState} from "react"
-import {useQuery} from "@apollo/client"
-import {QUERY_USER} from "../../utils/queries"
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import ProjectCarousel from '../../components/Carosel/Carosel';
 const ProjectPage = () => {
-  const {loading, data} = useQuery(QUERY_USER)
-  const userData = data?.user || {}
-  const projectsArray= userData.projects
-  console.log(projectsArray)
-  return (
-    <div>
-      <h1>Project Page Placeholder</h1>
-    </div>
-  )  
-}
+  const { loading, data } = useQuery(QUERY_USER);
+  // const userData = data?.user || {}
+  const projectsArray = data?.user.projects || [];
 
-export default ProjectPage
+  return (
+    <section>
+      <ProjectCarousel projects={projectsArray} />
+    </section>
+  );
+};
+
+export default ProjectPage;
