62af4107f7858a4218e9e8388d7c6b2c0a1e9388
Corey Yates <cqyates@gmail.com>
2024-06-07 17:22:03 -0400

    removed mongo uri from codebase

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/corey-connect');
-//mongoose.connect('mongodb+srv://cqyates:D3^3FYp45ynJN*F@cluster0.drpsv39.mongodb.net/corey-connect?retryWrites=true&w=majority&appName=Cluster0')
 module.exports = mongoose.connection;
