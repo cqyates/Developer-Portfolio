3c4a2cb31672616fe213e459375f285795925ea3
Corey Yates <cqyates@gmail.com>
2024-06-15 21:22:24 -0400

    added side navigation to pages and matched main style

--- client/src/components/MiniNav/index.jsx
+++ client/src/components/MiniNav/index.jsx
@@ -1,27 +1,30 @@
-import Nav from "react-bootstrap/Nav";
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-import "./style.css";
+import './style.css';
 const MiniNav = () => {
   return (
     <aside>
-      <Nav style={{display: "flex", flexDirection: "column"}}>
-      <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/projects">
-            Projects
-          </Nav.Link>
-          <Nav.Link as={Link} to="/recommendations">
-            Recommendations
-          </Nav.Link>
-          <Nav.Link as={Link} to="/resume">
-            Resume
-          </Nav.Link>
-          <Nav.Link as={Link} to="/contact">
-            Contact Me
-          </Nav.Link>
+      <Nav style={{ display: 'flex', flexDirection: 'column'}}>
+        <Nav.Link style={{color: "white"}} as={Link} to="/">
+          Home
+        </Nav.Link>
+        <Nav.Link style={{color: "white"}} as={Link} to="/about">
+          About Me
+        </Nav.Link>
+        <Nav.Link style={{color: "white"}} as={Link} to="/projects">
+          Projects
+        </Nav.Link>
+        <Nav.Link style={{color: "white"}} as={Link} to="/recommendations">
+          Recommendations
+        </Nav.Link>
+        <Nav.Link style={{color: "white"}} as={Link} to="/resume">
+          Resume
+        </Nav.Link>
+        <Nav.Link style={{color: "white"}} as={Link} to="/contact">
+          Contact Me
+        </Nav.Link>
       </Nav>
     </aside>
-  )
-}
-export default MiniNav
+  );
+};
+export default MiniNav;
