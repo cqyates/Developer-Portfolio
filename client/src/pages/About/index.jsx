2f32c3cc4cf76058dce0121cebecbaca4fdd4b13
Corey Yates <cqyates@gmail.com>
2024-07-27 17:28:24 -0400

    added cards to about me

--- client/src/pages/About/index.jsx
+++ client/src/pages/About/index.jsx
@@ -2,6 +2,7 @@ import MiniNav from '../../components/MiniNav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import CatPic from "../../assets/images/kamalaCat.png"
 //put inside a carousel?
 
 const About = () => {
@@ -61,10 +62,10 @@ const About = () => {
                 </Card.Title>
               </Card.Header>
               <Card.Body>
-                <Card.Text>Paragraph about teaching code</Card.Text>
+                <Card.Text>For the past 5 years, it has been my honor to support coding bootcamp students on their journey to learning the MERN stack.  It is inspiring to me to see many of my students go on to become developers at all levels, able to learn new languages with ease.  They say that you truly know a subject when you can teach that subject to another person and I've learned more as a teacher than as a student.  I've learn to identify not just bugs and errors but misconceptions and thinking traps. Its always my goal to leave my students with the logical foundation necessary to keep learning and growing as developers.  As my students have grown and spread their wings, they would bring me new technologies and methods that I would learn on the fly and then help them debug the issues and successful implement their feature.  As a result of this work, I am a flexible developer, a good mentor and a quick study.</Card.Text>
               </Card.Body>
             </Card>
-            <Card style={{ width: '40%', margin: '1em' }}>
+            {/* <Card style={{ width: '40%', margin: '1em' }}>
               <Card.Header>
                 <Card.Title><h3>Ecommerce Expert</h3></Card.Title>
               </Card.Header>
@@ -73,17 +74,8 @@ const About = () => {
                   Paragraph about growing an Ecommerce Business
                 </Card.Text>
               </Card.Body>
-            </Card>
-            <Card style={{ width: '40%', margin: '1em' }}>
-              <Card.Header>
-                <Card.Title><h3>Advocate</h3> </Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  Paragraph about advocating for women in tech{' '}
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            </Card> */}
+{/*            
             <Card style={{ width: '40%', margin: '1em' }}>
               <Card.Header>
                 <Card.Title><h3>Mentor</h3> </Card.Title>
@@ -91,13 +83,14 @@ const About = () => {
               <Card.Body>
                 <Card.Text>Paragraph about growing new developers </Card.Text>
               </Card.Body>
-            </Card>
+            </Card> */}
             <Card style={{ width: '40%', margin: '1em' }}>
               <Card.Header>
-                <Card.Title> <h3>Music Lover</h3> </Card.Title>
+                <Card.Title> <h3>Political Activist</h3> </Card.Title>
               </Card.Header>
               <Card.Body>
-                <Card.Text>Paragraph about growing new developers </Card.Text>
+                <Card.Text> I am passionate about advancing the cause of empowering women in the workplace.  I believe that diversity of experience and opinion makes our code and our lives stronger. I always try to boost strong female voices and have proudly volunteered for the Stacey Abrams campaign.  This year, this cat is for Kamala.  See you on the campaign trail! </Card.Text>
+                <Card.Img src={CatPic} />
               </Card.Body>
             </Card>
           </div>
